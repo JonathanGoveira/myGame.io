0eb63ecbac869255834962c9a7deb63650c6e1ee
JonathanGoncalvesDeOliveira <jonathan.g.oliveira13@gmail.com>
2019-12-06 02:50:46 -0300

    add getters and setters

--- source/Objects/instance.ts
+++ source/Objects/instance.ts
@@ -1,14 +1,14 @@
 import Vector2D from "../Utils/vectors"
 export default class Instance{
     protected position: Vector2D;
-    protected width: number;
-    protected height: number;
+    protected _width: number;
+    protected _height: number;
     protected angle: number = 0;
 
     constructor (p_position: Vector2D,p_w: number, p_h: number){
         this.position = p_position;
-        this.width = p_w;
-        this.height = p_h;
+        this._width = p_w;
+        this._height = p_h;
     }
 
     get x(){
@@ -17,5 +17,20 @@ export default class Instance{
 
     get y(){
         return this.position.y;
+    }
+
+    set x(p_x: number){
+        this.position.x = p_x
+    }
+
+    set y(p_y: number){
+        this.position.y = p_y
+    }
+    
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
     }
 }
